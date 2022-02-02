1fbdc021caaa2d1ce197477c95aed9dec8983ee9
monbofranco <monbofranco@hotmail.com>
2022-02-02 14:00:54 -0500

    aq

--- sw.js
+++ sw.js
@@ -1,76 +1,47 @@
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js').then(function(reg) {
-        // registration worked
-        console.log('Yes ! Enregsistrement réussi !. Scope is ' + reg.scope);
-    }).catch(function(error) {
-        // registration failed
-        console.log('Aie Aie erreur ' + error);
-      });
-    };
+this.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installation en cours de v1');
+    event.waitUntil(
+      caches.open('v1').then(function(cache) {
+        console.log('[Service Worker] Mise en cache globale: app shell et contenu et tout et tout');
+        return cache.addAll([
+            'index.html',
+            'style.css',
+            'index.js',
+            'banner1.jpg',
+            'banner.jpge',
+            'beginnerguide.jpeg',
+            'custom1.jpeg',
+            'icones/icon-512x512.png',
+            'bootstrap-5.1.3-dist/css/bootstrap.min.css',
+            'icons-1.7.2/font/bootstrap-icons.css',
+            'bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js',
+            'customv1.jpeg',
+            'custom1.jpeg',
+            'icone.jpg'
+        ]);
+      })
+    );
+  });
 
-
-// Initialise la variable pour l'utiliser plus tard afin d'afficher une invitation a installer la PWA
-
-
-let deferredPrompt;
- 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Empêcher la mini-barre d'information d'apparaître sur mobile si deja installée
-  e.preventDefault();
-  // Réserve l'événement pour qu'il puisse être déclenché plus tard
-  deferredPrompt = e;
-  // Informe l'utilisateur qu'il peut installer la PWA avec une fenêtre
-  installApp();
+  self.addEventListener('fetch', (event) => {
+    event.respondWith( caches.match(event.request).then((response) => {
+      if (response !== undefined) {
+        return response;
+      } 
+      else {
+        return fetch(event.request).then(function (response) {
+          let responseClone = response.clone();
   
-  console.log(`'beforeinstallprompt' a été déclenché !`);
-
-  // Fonction qui renvoie a l'installation de l'application quand le boutton "Installer" est cliqué !
-    appButton.addEventListener('click', function() {
-      deferredPrompt.prompt();
-    })
-
-});
-
-// Fonction qui affiche une fenêtre a l'ouverture de la page pour informer l'utilisateur de la possibilité d'installer cette PWA
-// Possible de faire autrement, mais le toast de bootstrap est assez simple a utiliser
-
-
-function installApp() {
-  console.log('hey');
-  const fenetre = document.getElementById('message')
-  const toast = new bootstrap.Toast(fenetre, {delay: 6000}) //reste affichée 6 secondes seulement
-  toast.show();
-};  
-const filesToCache = [
-  '/',
-  'style.css',
-  'banner.jpeg',
-  'index.html',
-  'pages/offline.html',
-  'pages/404.html'
-];
-
-const staticCacheName = 'pages-cache-v1';
-
-self.addEventListener('install', event => {
-  console.log('Attempting to install service worker and cache static assets');
-  event.waitUntil(
-    caches.open(staticCacheName)
-    .then(cache => {
-      return cache.addAll(filesToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.open('mysite-dynamic').then(function(cache) {
-      return cache.match(event.request).then(function (response) {
-        return response || fetch(event.request).then(function(response) {
-          cache.put(event.request, response.clone());
+          caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+  
           return response;
         });
-      });
-    })
-  );
+      }
+    }));
+  });
+
+  this.addEventListener('activate', (e) => {
+  console.log('Service worker activé, all good !');
 });
