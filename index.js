f337f3f549948c2f3e76b52f508dcab3f5e498d1
monbofranco <monbofranco@hotmail.com>
2022-02-02 14:41:59 -0500

    asd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 if('serviceWorker' in navigator) {
     navigator.serviceWorker.register('sw.js').then(function(reg) {
         // registration worked
-        console.log('Yes ! Enregsistrement réussi !. Scope is ' + reg.scope);
+        console.log('Yes ! Enregistrement réussi !. Scope is ' + reg.scope);
     }).catch(function(error) {
         // registration failed
         console.log('Aie Aie erreur ' + error);
@@ -10,8 +10,6 @@ if('serviceWorker' in navigator) {
 
 
 // Initialise la variable pour l'utiliser plus tard afin d'afficher une invitation a installer la PWA
-
-
 let deferredPrompt;
  
 window.addEventListener('beforeinstallprompt', (e) => {
@@ -34,11 +32,9 @@ window.addEventListener('beforeinstallprompt', (e) => {
 // Fonction qui affiche une fenêtre a l'ouverture de la page pour informer l'utilisateur de la possibilité d'installer cette PWA
 // Possible de faire autrement, mais le toast de bootstrap est assez simple a utiliser
 
-
 function installApp() {
-  console.log('hey');
+  console.log('hey ma fenetre marche!');
   const fenetre = document.getElementById('message')
   const toast = new bootstrap.Toast(fenetre, {delay: 6000}) //reste affichée 6 secondes seulement
   toast.show();
 };
-
